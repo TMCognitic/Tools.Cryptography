d6a5d2e012660962a116da6d0b38dc90ae6ef651
Morre Thierry <thierry.morre@cognitic.be>
2022-07-08 12:33:40 +0200

    Ajoutez des fichiers projet.

---
+++ Tools.Cryptography/CryptoAes.cs
@@ -0,0 +1,66 @@
+﻿using System.Security.Cryptography;
+
+namespace Tools.Cryptography
+{
+    public sealed class CryptoAes
+    {
+        private readonly Aes _aesProvider;
+
+        public CryptoAes()
+        {
+            _aesProvider = Aes.Create();
+            _aesProvider.GenerateIV();
+            _aesProvider.GenerateKey();
+        }
+
+        public CryptoAes(byte[] vector, byte[] key)
+        {
+            _aesProvider = Aes.Create();
+            _aesProvider.IV = vector;
+            _aesProvider.Key = key;
+        }
+
+        public byte[] IV
+        {
+            get { return _aesProvider.IV; }
+        }
+
+        public byte[] Key
+        {
+            get { return _aesProvider.Key; }
+        }
+
+        public byte[] Encrypt(string toEncrypt)
+        {
+            ICryptoTransform encryptor = _aesProvider.CreateEncryptor(Key, IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(toEncrypt);
+                    }
+                    return msEncrypt.ToArray();
+                }
+            }
+        }
+
+        public string Decrypt(byte[] cypher)
+        {
+            ICryptoTransform decryptor = _aesProvider.CreateDecryptor(Key, IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(cypher))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Tools.Cryptography/CryptoRSA.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Tools.Cryptography
+{
+    public class CryptoRSA
+    {
+        private readonly RSACryptoServiceProvider _serviceProvider;
+
+        public CryptoRSA(in int keySize)
+        {
+            _serviceProvider = new RSACryptoServiceProvider(keySize);            
+        }
+
+        public CryptoRSA(in byte[] keyBlob)
+        {
+            _serviceProvider = new RSACryptoServiceProvider();
+            _serviceProvider.ImportCspBlob(keyBlob);
+        }
+
+        public int KeySize
+        {
+            get { return _serviceProvider.KeySize; }
+        }
+
+        public int MaxContentSize
+        {
+            //Key Size in Byte - 2 * Hash Size in octet - 2
+            get { return (KeySize / 8) - (2 * 20) - 2; }
+        }
+
+        public bool PublicKeyOnly
+        {
+            get { return _serviceProvider.PublicOnly; }
+        }
+
+        public string ToXml(in bool includePrivateKey)
+        {
+            return _serviceProvider.ToXmlString(includePrivateKey);
+        }
+
+        public byte[] ToByteArray(in bool includePrivateKey)
+        {
+            return _serviceProvider.ExportCspBlob(includePrivateKey);
+        }
+
+        public byte[] Encrypt(in string content)
+        {
+            if (content.Length > MaxContentSize)
+                throw new InvalidOperationException($"With the current key you can encrypt a string with max size : {MaxContentSize}");
+
+            byte[] toEncode = Encoding.Unicode.GetBytes(content);
+            return _serviceProvider.Encrypt(toEncode, true);
+        }
+
+        public string Decrypt(in byte[] cypher)
+        {
+            if (PublicKeyOnly)
+                throw new InvalidOperationException("Only the private key can decrypt.");
+
+            byte[] decodedData = _serviceProvider.Decrypt(cypher, true);
+            return Encoding.Unicode.GetString(decodedData);
+        }
+
+        public byte[] Sign(in byte[] cypher)
+        {
+            if (PublicKeyOnly)
+                throw new InvalidOperationException("Only the private key can sign.");
+
+            return _serviceProvider.SignData(cypher, SHA512.Create());
+        }
+
+        public bool Verify(in byte[] cypher, in byte[] signedData)
+        {
+            return _serviceProvider.VerifyData(cypher, SHA512.Create(), signedData);
+        }
+    }
+}
